9891053aaa85a595058c23d6146e8ed0c6968274
Mohammad Joubeh <work.jobeh@gmail.com>
2023-08-08 11:09:35 +0430

    Optimized unique validator

--- app/Validator/UniqueValidator.php
+++ app/Validator/UniqueValidator.php
@@ -7,8 +7,8 @@ use App\Router\Abort;
 class UniqueValidator implements IsValidator
 {
     public function validate($key, $data, $extera = null){
-        $user = $extera::where('email', $data['email'])->get();
-        if(count($user) != 0){
+        $user = $extera::where($key, $data[$key])->count();
+        if($user != 0){
             Abort::badRequest($key.' already exists on '.$extera);
         }
     }
